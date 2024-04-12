95772728e0a2c5d7db9b2c76cca9edb1c788a522
Jonathan Mikler <s222962@dtu.dk>
2024-04-12 11:00:41 +0200

    Update PS1_config.sh to customize colors in the PS1 prompt

--- shell_addons/PS1_config.sh
+++ shell_addons/PS1_config.sh
@@ -3,11 +3,25 @@
 
 # This script is used to configure the PS1
 
+
+
 main(){
+
+    local RED='\033[01;31m'
+    local DARK_RED='\033[00;31m'
+    local GREEN='\033[01;32m'
+    local DARK_GREEN='\033[00;32m'
+    local YELLOW='\033[01;33m'
+    local BLUE='\033[01;34m'
+    local NO_COLOR='\033[00m'
+
     source "${GENERIC_SHELL_TOOLBOX_LOCATION}/shell_addons/git_utils.sh"
 
 
-    export PS1="$PS1\[\033[01;31m\]\$(get_repository_name):\$(parse_git_branch)\[\033[00m\] \n> "
+    local GIT_BRANCH_COLOR="${RED}"
+    local GIT_REPO_COLOR="${DARK_RED}"
+
+    export PS1="$PS1${GIT_REPO_COLOR}(\$(get_repository_name):${GIT_BRANCH_COLOR}$(parse_git_branch))${NO_COLOR} \n> "
 
 }
 
