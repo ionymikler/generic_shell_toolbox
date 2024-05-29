f61e1de71b4e53c2920cdcc7fa908dfd6190d178
Jonathan Mikler <ionymikler@gmail.com>
2024-05-30 01:13:01 +0200

    cleanup

--- install.bash
+++ install.bash
@@ -9,36 +9,41 @@ source "${SCRIPT_PATH}/.make_env_file.sh"
 
 function set_PS1(){
     # decide if including a default PS1 configuration
-    read -p "Do you want to include a default PS1 configuration? (y/n): " include_PS1
-
-    if [ "$include_PS1" == "y" ]; then
-        local default_PS1="\[\e[38;5;47m\]\u\[\e[38;5;156m\]@\[\e[38;5;227m\]\h \[\e[38;5;231m\]\w \[\033[0m\]$ "
-        echo "export PS1='$default_PS1'" >> "$script_dir/.env"
-        echo "The default PS1 has been added to the .env file."
-    fi
-    echo ""
+    local default_PS1="\[\e[38;5;47m\]\u\[\e[38;5;156m\]@\[\e[38;5;227m\]\h \[\e[38;5;231m\]\w \[\033[0m\]$ "
+    echo "export PS1='$default_PS1'" >> "$SCRIPT_PATH/.env"
+    echo "The default PS1 has been added to the .env file."
 }
 
 function add_to_bashrc(){
-    read -p "Do you want to add the 'generic_shell_toolbox.sh' to your bashrc file? (y/n): " paste_to_bashrc
+    local _sourcing_file_path="$HOME/.bashrc"
+    echo "source ${GENERIC_SHELL_TOOLBOX_LOCATION}/generic_shell_toolbox.sh" >> $_sourcing_file_path
 
-    if [ "$paste_to_bashrc" == "y" ]; then
-        echo "source ${GENERIC_SHELL_TOOLBOX_LOCATION}/generic_shell_toolbox.sh" >> ~/.bashrc
-        echo "The 'generic_shell_toolbox.sh' has been added to your bashrc file. Open a new terminal to see the changes."
-    fi
+    # remember in env file
+    echo "SOURCING_FILE_PATH=$_sourcing_file_path" >> "$SCRIPT_PATH/.env"
+
+    log_info_green "The 'generic_shell_toolbox.sh' has been added to your bashrc file. Open a new terminal to see the changes."
 }
 
-main(){
+install_gst(){
     log_info_blue "Installing the 'generic_shell_toolbox'... \n"
 
     make_env_file
 
-    set_PS1
-    add_to_bashrc
+    # decide if including a default PS1 configuration
+    read -p "* Do you want to include a default PS1 configuration? (y/n): " include_PS1
+    if [ "$include_PS1" == "y" ]; then
+        set_PS1
+    fi
 
-    printf "Installation complete.\n"
+    # decide if adding the 'generic_shell_toolbox.sh' to the bashrc file
+    read -p "* Do you want to add the 'generic_shell_toolbox.sh' to your bashrc file? (y/n): " paste_to_bashrc
+    if [ "$paste_to_bashrc" == "y" ]; then
+        add_to_bashrc
+    fi
+
+    log_info_green "Installation complete"
 
 }
 
-main
+install_gst
 
