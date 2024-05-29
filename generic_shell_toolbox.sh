ba61c586b27a815bd075406daacbaf832053b68e
Jonathan Mikler <ionymikler@gmail.com>
2024-05-30 00:16:46 +0200

    chore: Refactor enabling.sh and generic_shell_toolbox.sh for better readability and maintainability

--- generic_shell_toolbox.sh
+++ generic_shell_toolbox.sh
@@ -5,32 +5,11 @@ function verify_env_file(){
     script_dir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
     if [ -f "${script_dir}/.env" ]; then
         source "${script_dir}/.env"
+        
     else
         echo "Error .env '${script_dir}/.env' file does not exist, please run the '.get_toolbox_location.sh' script"
         return 1
     fi
-}
-
-function enable_toolbox(){
-    # script dir is the first argument passed to the function
-    local _script_dir=$1
-
-    if [ -f "${_script_dir}/.env" ]; then
-        # if the .env file exists, check if the TOOLBOX_ENABLED variable exists, if so, overwrite it to 'true'
-        # If it does not exists, create it and set it to 'true'
-        source "${_script_dir}/.env"
-        if [ -z "${TOOLBOX_ENABLED}" ]; then
-            # echo "writing TOOLBOX_ENABLED=true to .env file"
-            echo "TOOLBOX_ENABLED=true" >> "${_script_dir}/.env"
-        else
-            sed -i 's/TOOLBOX_ENABLED=.*/TOOLBOX_ENABLED=true/' "${_script_dir}/.env"
-        fi
-        echo "Generic-ToolBox enabled"
-    else
-        echo "Error .env '${_script_dir}/.env' file does not exist, please run the '.get_toolbox_location.sh' script"
-        return 1
-    fi
-
 }
 
 function source_core(){
