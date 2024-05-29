9e3e30d6c616074c073f5f4abaa946096658f614
Jonathan Mikler <ionymikler@gmail.com>
2024-05-30 01:15:55 +0200

    cleanup

--- shell_utils/aliases.sh
+++ shell_utils/aliases.sh
@@ -2,10 +2,18 @@
 # Created by Jonathan Mikler on 29/February/24
 
 # this script is used to define aliases
-
-main(){
-alias rebash="log_info 'resourcing ~/.bashrc'&&source ~/.bashrc"
-
+function gst_version(){
+    # echo version from file ${GENERIC_SHELL_TOOLBOX_LOCATION}/version.yaml
+    local version_file="${GENERIC_SHELL_TOOLBOX_LOCATION}/version.yaml"
+    if [ -f "$version_file" ]; then
+        cat "$version_file"
+    else
+        log_error "No version file found."
+    fi
 }
 
-main
+
+function rebash(){
+    log_info 'resourcing ~/.bashrc'
+    source "~/.bashrc"
+}
