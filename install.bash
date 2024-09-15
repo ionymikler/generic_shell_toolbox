7da13688cac58aef219bcc6a74c0218bd77f81a5
Jonathan Mikler <ionymikler@gmail.com>
2024-09-15 15:45:39 +0200

    default flag for no prompt installation

--- install.bash
+++ install.bash
@@ -8,7 +8,6 @@ source "${SCRIPT_PATH}/shell_utils/colored_shell.sh"
 source "${SCRIPT_PATH}/.make_env_file.sh"
 
 function set_PS1(){
-    # decide if including a default PS1 configuration
     local default_PS1="\[\e[38;5;47m\]\u\[\e[38;5;156m\]@\[\e[38;5;227m\]\h \[\e[38;5;231m\]\w \[\033[0m\]$ "
     echo "export PS1='$default_PS1'" >> "$SCRIPT_PATH/.env"
     echo "The default PS1 has been added to the .env file."
@@ -25,6 +24,14 @@ function add_to_bashrc(){
 }
 
 install_gst(){
+    local SET_DEFAULT_CONFIG='false'
+
+    log_debug "$@"
+    if [[ "$@" == *"-D"* ]]; then
+        SET_DEFAULT_CONFIG=true
+    fi
+    log_debug "SET_DEFAULT_CONFIG: $SET_DEFAULT_CONFIG"
+
     # read version from dir_path/version.yaml
     local _version=$(cat "${SCRIPT_PATH}/version.yaml")
     log_info_blue "Installing the 'generic_shell_toolbox' $_version... \n"
@@ -32,13 +39,26 @@ install_gst(){
     make_env_file
 
     # decide if including a default PS1 configuration
-    read -p "* Do you want to include a default PS1 configuration? (y/n): " include_PS1
+    local include_PS1="n"
+    
+    if [ "$SET_DEFAULT_CONFIG" == "true" ]; then
+        include_PS1="y"
+    else
+        read -p "* Do you want to include a default PS1 configuration? (y/n): " include_PS1
+    fi
     if [ "$include_PS1" == "y" ]; then
         set_PS1
     fi
 
     # decide if adding the 'generic_shell_toolbox.sh' to the bashrc file
-    read -p "* Do you want to add the 'generic_shell_toolbox.sh' to your bashrc file? (y/n): " paste_to_bashrc
+    local paste_to_bashrc="n"
+    
+    if [ "$SET_DEFAULT_CONFIG" == "true" ]; then
+        paste_to_bashrc="y"
+    else
+        read -p "* Do you want to add the 'generic_shell_toolbox.sh' to your bashrc file? (y/n): " paste_to_bashrc
+    fi
+
     if [ "$paste_to_bashrc" == "y" ]; then
         add_to_bashrc
     fi
@@ -47,5 +67,5 @@ install_gst(){
 
 }
 
-install_gst
+install_gst "$@"
 
